6f4ed6ff801a729b0b34769579d6aeb170bc7fb8
Weegoos <batyr.ashim05@mail.ru>
2025-03-07 12:29:04 +0500

    Re-added client directory properly

---
+++ client/src/pages/IndexPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>{{ data }}</div>
+</template>
+
+<script setup>
+import axios from "axios";
+import { onMounted, ref } from "vue";
+
+const data = ref(null)
+const getMessage = async () => {
+  try {
+    const response = await axios.get("http://localhost:3000/api/message", {
+      headers: {
+        "Content-Type": "appliction/json",
+              },
+      withCredentials: true,
+    });
+    console.log(response.data);
+    data.value = response.data.message
+  } catch (error) {
+    console.error(error);
+
+  }
+};
+
+onMounted(() => {
+  getMessage()
+})
+</script>
+
+<style></style>
