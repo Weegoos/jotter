4cb3ba12af60d6d516b1c40362bbdb819aab3f19
Weegoos <batyr.ashim05@mail.ru>
2025-03-17 12:05:38 +0500

    Jotter: The project has been updated

--- server/routers/fileRouters.js
+++ server/routers/fileRouters.js
@@ -3,7 +3,61 @@ import { createFile, getFilesByUserId, getFilesName } from "../controllers/fileC
 import authMiddleware from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Files
+ *   description: API для управления файлами
+ */
+
+
 router.post('/create',authMiddleware,  createFile)
+
+// -------------------- file/create -----------------------
+
+/**
+ * @swagger
+ * /file/create:
+ *   post:
+ *     summary: Создать новую папку
+ *     tags: [Files]
+ *     description: Добавляет новый файл в базу данных.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: "Новый Файл"
+ *     responses:
+ *       201:
+ *         description: Файл успешно создан
+ *         name:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Файл создан!"
+ *                 file:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: integer
+ *                       example: 1
+ *                     name:
+ *                       type: string
+ *                       example: "Новый файл"
+ *                     createdAt:
+ *                       type: string
+ *                       format: date-time
+ *                       example: "2024-03-07T12:34:56.000Z"
+ */
+
 
 router.get('/allFiles', authMiddleware, getFilesByUserId)
 
