3b5e73f2130eadb63b9e7dbbe7ad9324da264cc3
Weegoos <batyr.ashim05@mail.ru>
2025-03-20 01:57:24 +0500

    Jotter: The ViewFilesPage.vue has been updated

--- client/src/components/Files/ViewFilesPage.vue
+++ client/src/components/Files/ViewFilesPage.vue
@@ -1,11 +1,74 @@
 <template>
   <div class="q-pa-md">
-    File
+    <div class="q-pa-md" data-testid="rowsID" v-if="rows && rows.length">
+    <q-table
+      data-testid="userTable"
+      flat
+      bordered
+      :rows="rows"
+      :columns="columns"
+      row-key="id"
+      @row-click="viewAllNotes"
+      hide-bottom
+    />
+  </div>
+  <div v-else data-testid="noData">
+    <p>No data</p>
+  </div>
   </div>
 </template>
 
-<script>
-export default {
+<script setup>
+import { getMethod } from 'src/composables/api/getApi';
+import { getCurrentInstance, onMounted, ref } from 'vue';
+
+// global variables
+const {proxy} = getCurrentInstance()
+const serverURL = proxy.$serverURL
+
+const columns = [
+  {
+    name: "id",
+    label: "ID",
+    align: "left",
+    field: `id`,
+    sortable: true,
+  },
+  {
+    name: "name",
+    label: "Name",
+    align: "left",
+    field: `name`,
+    sortable: true,
+  },
+  {
+    name: "createdAt",
+    label: "Created At",
+    align: "left",
+    field: `createdAt`,
+    sortable: true,
+  },
+  {
+    name: "updatedAt",
+    label: "Updated At",
+    align: "left",
+    field: `updatedAt`,
+    sortable: true,
+  },
+];
+
+const rows = ref([])
+
+const allFilesRelatedToThisUser = async () => {
+  await getMethod(serverURL, 'file/allFiles', rows, null)
+}
+
+onMounted(() => {
+  allFilesRelatedToThisUser()
+})
+
+const viewAllNotes = (evt, row, index) => {
+  console.log(row.id);
 
 }
 </script>
