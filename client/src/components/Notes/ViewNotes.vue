f1a0396d41e31b8d25fb395b94f88723216a7607
Weegoos <batyr.ashim05@mail.ru>
2025-03-20 10:27:26 +0500

    Jotter: The design of the ViewNotes.vue has been updated

--- client/src/components/Notes/ViewNotes.vue
+++ client/src/components/Notes/ViewNotes.vue
@@ -1,21 +1,39 @@
 <template>
   <div class="q-pa-md">
-    <q-card
-      class="my-card q-ma-sm"
-      v-for="(notes, id) in allNotesByFileId"
-      :key="id"
-    >
-      <q-card-section>
-        <div class="text-h6">{{ notes.title }}</div>
-      </q-card-section>
-      <q-card-section>
-        {{ notes.content }}
-      </q-card-section>
-      <q-card-actions align="right">
-        <q-btn flat icon="edit" @click="openEditPage(notes.id)" />
-        <q-btn flat color="red" icon="delete" @click="deleteNote(notes.id)" />
-      </q-card-actions>
-    </q-card>
+    <q-scroll-area style="height: 70px">
+      <div class="row no-wrap">
+        <div v-for="(types, index) in allTypes" :key="index" class="q-pa-sm">
+          <q-btn
+            no-caps
+            flat
+            dense
+            class="scroll-button"
+            color="primary"
+            :label="types.name"
+            @click="getCurrentTypeNotes"
+          />
+        </div>
+      </div>
+    </q-scroll-area>
+
+    <q-scroll-area style="height: 100vh">
+      <q-card
+        class="my-card q-ma-sm"
+        v-for="(notes, id) in allNotesByFileId"
+        :key="id"
+      >
+        <q-card-section>
+          <div class="text-h6">{{ notes.title }}</div>
+        </q-card-section>
+        <q-card-section>
+          {{ notes.content }}
+        </q-card-section>
+        <q-card-actions align="right">
+          <q-btn flat icon="edit" @click="openEditPage(notes.id)" />
+          <q-btn flat color="red" icon="delete" @click="deleteNote(notes.id)" />
+        </q-card-actions>
+      </q-card>
+    </q-scroll-area>
     <EditNote
       :isOpenEditPage="isOpenEditPage"
       :noteIdentification="noteIdentification"
@@ -34,16 +52,25 @@ import { deleteMethod } from "src/composables/api/delete";
 const { proxy } = getCurrentInstance();
 const serverURL = proxy.$serverURL;
 
+const panel = ref("mails");
+
 const getIdFromUrl = () => {
   const url = window.location.href;
   const parts = url.split("/");
   return parts[parts.length - 1];
 };
 
+const allTypes = ref([]);
+const getAllTypes = async () => {
+  await getMethod(serverURL, "types", allTypes, null);
+  console.log(allTypes.value);
+};
+
 let ws = null;
 const allNotesByFileId = ref([]);
 onMounted(() => {
-  getAllNotesByFileId(); // Загружаем заметки при загрузке страницы
+  getAllNotesByFileId();
+  getAllTypes();
 
   // 🔌 Подключаем WebSocket
   ws = new WebSocket("ws://localhost:3000"); // ⚠ Укажи свой WebSocket URL
@@ -78,6 +105,8 @@ onMounted(() => {
   };
 });
 
+const getCurrentTypeNotes = () => {};
+
 const id = getIdFromUrl();
 const getAllNotesByFileId = async () => {
   getMethod(
@@ -104,4 +133,10 @@ const deleteNote = (noteId) => {
 };
 </script>
 
-<style></style>
+<style scoped>
+.scroll-button {
+  border: solid 1px green;
+  border-radius: 15px;
+  width: 70px;
+}
+</style>
