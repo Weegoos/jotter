f7ac46e03b52677518e2598a629f007828fd0559
Weegoos <batyr.ashim05@mail.ru>
2025-06-18 23:08:24 +0500

    Jotter: The delete and put functions related to file have been updated

---
+++ server/controllers/file/deleteFileControllers.js
@@ -0,0 +1,42 @@
+import Files from "../../schemas/fileSchemas.js";
+import { wss } from "../../server.js";
+
+export const deleteFileById = async (req, res) => {
+    try {
+        const {fileId} = req.params
+
+        if (!fileId) {
+            return res.status(400).json({ message: "Ошибка: fileId отсутствует." });
+        }
+
+        const file = await Files.findByPk(fileId)
+        if (!file) {
+            return res.status(404).json({ message: "Файл не найден." });
+        }
+
+        await file.destroy()
+
+        wss.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN){
+                client.send(JSON.stringify({
+                    event: "deleteFileByID",
+                    file: file
+                }))
+            }
+        })
+        res.status(200).json({ message: "Файл успешно удален." });
+    } catch (error) {
+        console.error("Ошибка при удалении файла:", error);
+        res.status(500).json({ message: "Ошибка сервера." });
+    }
+}
+
+export const deleteAllFiles = async (req, res) => {
+    try {
+        await Files.destroy({ where: {} });
+        res.status(200).json({ message: "Все файлы успешно удалены." });
+    } catch (error) {
+        console.error("Ошибка при удалении всех файлов:", error);
+        res.status(500).json({ message: "Ошибка сервера." });
+    }
+};
