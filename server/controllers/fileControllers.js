d5fd96d52274a76b2490a0f5050c4787d165eacc
Weegoos <batyr.ashim05@mail.ru>
2025-06-18 22:45:15 +0500

    Jotter: The getFileControllers.js has been created and get functions have been integrated to this file

--- server/controllers/fileControllers.js
+++ server/controllers/fileControllers.js
@@ -33,74 +33,7 @@ export const createFile = async (req, res) => {
     }
 }
 
-export const getFilesByUserId = async (req, res) => {
-    try {
-        const userId = req.user.id
-        if (!userId) {
-            return res.status(400).json({ message: "Ошибка: userId отсутствует." });
-        }
-        const files = await Files.findAll({ where: { userId } });
-   
-        wss.clients.forEach(client => {
-            if (client.readyState === 1){
-                client.send(JSON.stringify({event: "get_files", userId, files}))
-            }
-        })
-        res.json(files);
-    } catch (error) {
-        console.error("Ошибка:", error);
-        res.status(500).json({ message: "Ошибка сервера" });
-    }
-}
-export const getFilesByStatus = async (req, res) => {
-    try {
-        const userId = req.user?.id;
-        const { status } = req.query;
-        const page = parseInt(req.query.page) || 1;  
-        const limit = parseInt(req.query.limit) || 10;  
 
-        if (!userId) {
-            return res.status(400).json({ message: "Ошибка: userId отсутствует." });
-        }
-
-        if (!status) {
-            return res.status(400).json({ message: "Ошибка: статус обязателен." });
-        }
-
-        // Найдем файлы с учетом пагинации
-        const offset = (page - 1) * limit;
-
-        const files = await Files.findAndCountAll({
-            where: { userId, status },
-            limit: limit,
-            offset: offset
-        });
-
-        const totalPages = Math.ceil(files.count / limit);
-
-        wss.clients.forEach(client => {
-        if (client.readyState === 1 && client.userId === userId) {
-            client.send(JSON.stringify({
-            event: "get_files_by_status",
-            files: files.rows,
-            totalCount: files.count,
-            totalPages,
-            currentPage: page,
-            }));
-        }
-        });
-
-        res.json({
-            files: files.rows,
-            totalCount: files.count,
-            totalPages: totalPages,
-            currentPage: page,
-        });
-    } catch (error) {
-        console.error("Ошибка:", error);
-        res.status(500).json({ message: "Ошибка сервера" });
-    }
-};
 
 export const editFileStatus = async (req, res) => {
     try {
@@ -130,26 +63,6 @@ export const editFileStatus = async (req, res) => {
         });
 
         res.json({ message: "Статус обновлен", file });
-    } catch (error) {
-        console.error("Ошибка:", error);
-        res.status(500).json({ message: "Ошибка сервера" });
-    }
-};
-
-
-export const getFilesName = async (req, res) => {
-    try {
-        const userId = req.user.id;
-        if (!userId) {
-            return res.status(400).json({ message: "Ошибка: userId отсутствует." });
-        }
-        
-        const files = await Files.findAll({ 
-            where: { userId },
-            attributes: ['name'] 
-        });
-
-        res.json(files.map(file => file.name)); 
     } catch (error) {
         console.error("Ошибка:", error);
         res.status(500).json({ message: "Ошибка сервера" });
--- server/routers/fileRouters.js
+++ server/routers/fileRouters.js
@@ -1,18 +1,18 @@
 import express from "express";
-import { createFile, deleteAllFiles, deleteFileById, editFileStatus, getFilesByStatus, getFilesByUserId, getFilesName } from "../controllers/fileControllers.js";
 import authMiddleware from "../middlewares/authMiddleware.js";
 import "./swagger/fileSwagger.js"; 
+import { getFilesByStatus, getFilesByUserId, getFilesName } from "../controllers/file/getFileControllers.js";
 const router = express.Router();
 
 
-router.post('/create',authMiddleware,  createFile)
+// router.post('/create',authMiddleware,  createFile)
 
 router.get('/allFiles', authMiddleware, getFilesByUserId)
 router.get('/filesName', authMiddleware, getFilesName)
 router.get('/filesStatus', authMiddleware, getFilesByStatus)
 
-router.put('/editStatus', authMiddleware, editFileStatus)
+// router.put('/editStatus', authMiddleware, editFileStatus)
 
-router.delete('/deleteFile/:fileId', authMiddleware, deleteFileById)
-router.delete('/deleteAll', authMiddleware, deleteAllFiles)
+// router.delete('/deleteFile/:fileId', authMiddleware, deleteFileById)
+// router.delete('/deleteAll', authMiddleware, deleteAllFiles)
 export default router;
