cf9649e347e896769538ac4487c54e300cb9ee1c
Weegoos <batyr.ashim05@mail.ru>
2025-06-23 14:46:26 +0500

    Jotter: The DocumentTable.vue has been updated

---
+++ jotter/src/components/molecules/DocumentTable.vue
@@ -0,0 +1,124 @@
+<template>
+    <q-table
+      class="m-[8px]"
+      bordered
+      :title="props.title"
+      :rows="props.notes"
+      :columns="columns"
+      row-key="name"
+      v-if="props.notes?.length"
+    >
+      <template v-slot:body-cell-name="props">
+        <q-td :props="props">
+          <div
+            v-html="props.row.name"
+            class="max-w-[150px] whitespace-nowrap overflow-hidden text-ellipsis"
+          ></div>
+        </q-td>
+      </template>
+      <template v-slot:body-cell-description="props">
+        <q-td :props="props">
+          <div
+            v-html="props.row.description"
+            class="max-w-[150px] whitespace-nowrap overflow-hidden text-ellipsis"
+          ></div>
+        </q-td>
+      </template>
+      <template v-slot:body-cell-actions="props">
+        <q-td align="center">
+          <q-btn-dropdown @click.stop color="primary">
+            <q-list style="min-width: 100px">
+              <q-item clickable v-close-popup>
+                <q-item-section>
+                  <q-btn
+                    class="bg-amber-500 hover:bg-amber-600 text-white"
+                    icon="mdi-pin"
+                    size="sm"
+                    @click="emit('pinNote', props.row)"
+                  />
+                </q-item-section>
+              </q-item>
+              <q-item clickable>
+                <q-item-section>
+                  <q-btn
+                    class="bg-blue-500 hover:bg-blue-600 text-white"
+                    icon="mdi-pencil"
+                    size="sm"
+                    @click="emit('updateNote', props.row)"
+                  />
+                </q-item-section>
+              </q-item>
+              <q-item clickable v-close-popup>
+                <q-item-section>
+                  <q-btn
+                    class="bg-rose-500 hover:bg-rose-600 text-white"
+                    icon="mdi-delete"
+                    size="sm"
+                    @click="emit('deleteNote', props.row)"
+                  />
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </q-btn-dropdown>
+        </q-td>
+      </template>
+    </q-table>
+</template>
+
+<script setup>
+import { useDateFormat } from "src/composables/javascript-function/formatDate";
+import { computed } from "vue";
+
+// global variables
+const props = defineProps({
+  notes: {
+    type: Object,
+    required: true,
+  },
+  title: String,
+});
+
+const emit = defineEmits([
+  "pinNote",
+  "updateNote",
+  "deleteNote",
+  "viewDetailedInfoAboutNote",
+]);
+
+const columns = computed(() => [
+  {
+    name: "title",
+    label: "Name",
+    align: "left",
+    field: (row) => row.title,
+    sortable: true,
+  },
+  {
+    name: "type",
+    label: "Type",
+    align: "left",
+    field: (row) => row.type,
+    sortable: true,
+  },
+  {
+    name: "created_at",
+    label: "Created At",
+    align: "left",
+    field: (row) => useDateFormat(row.createdAt),
+    sortable: true,
+  },
+  {
+    name: "updated_at",
+    label: "Updated At",
+    align: "left",
+    field: (row) => useDateFormat(row.updatedAt),
+    sortable: true,
+  },
+  {
+    name: "actions",
+    label: "Actions",
+    align: "center",
+    field: "id",
+  },
+]);
+</script>
--- jotter/src/pages/Document/ViewDocument.vue
+++ jotter/src/pages/Document/ViewDocument.vue
@@ -1,136 +1,24 @@
 <template>
   <div>
-        <q-table
-      class="m-[8px]"
-      bordered
-      title="Закрепленные заметки"
-      :rows="pinnedNote"
+    <DocumentTable
+      :notes="Object(pinnedNote)"
+      :title="String('Закрепленные заметки')"
+      @deleteNote="deleteNote"
+      @pinNote="pinNote"
+      @updateNote="updateNote"
+      @viewDetailedInfoAboutNote="viewDetailedInfoAboutNote"
+       @row-click="viewDetailedInfoAboutNote"
+    />
+
+    <DocumentTable
+      :notes="Object(rows)"
+      :title="String('Заметки')"
+      @deleteNote="deleteNote"
+      @pinNote="pinNote"
+      @updateNote="updateNote"
       @row-click="viewDetailedInfoAboutNote"
-      :columns="columns"
-      row-key="name"
-      hide-bottom
-      v-if="pinnedNote?.length"
-    >
-      <template v-slot:body-cell-name="props">
-        <q-td :props="props">
-          <div
-            v-html="props.row.name"
-            class="max-w-[150px] whitespace-nowrap overflow-hidden text-ellipsis"
-          ></div>
-        </q-td>
-      </template>
-      <template v-slot:body-cell-description="props">
-        <q-td :props="props">
-          <div
-            v-html="props.row.description"
-            class="max-w-[150px] whitespace-nowrap overflow-hidden text-ellipsis"
-          ></div>
-        </q-td>
-      </template>
-      <template v-slot:body-cell-actions="props">
-        <q-td align="center">
-          <q-btn-dropdown @click.stop color="primary">
-            <q-list style="min-width: 100px">
-              <q-item clickable v-close-popup>
-                <q-item-section>
-                  <q-btn
-                    class="bg-amber-500 hover:bg-amber-600 text-white"
-                    icon="mdi-pin"
-                    size="sm"
-                    @click="pinNote(props.row)"
-                  />
-                </q-item-section>
-              </q-item>
-              <q-item clickable>
-                <q-item-section>
-                  <q-btn
-                    class="bg-blue-500 hover:bg-blue-600 text-white"
-                    icon="mdi-pencil"
-                    size="sm"
-                    @click="updateNote(props.row)"
-                  />
-                </q-item-section>
-              </q-item>
-              <q-item clickable v-close-popup>
-                <q-item-section>
-                  <q-btn
-                    class="bg-rose-500 hover:bg-rose-600 text-white"
-                    icon="mdi-delete"
-                    size="sm"
-                    @click="deleteNote(props.row)"
-                  />
-                </q-item-section>
-              </q-item>
-            </q-list>
-          </q-btn-dropdown>
-        </q-td>
-      </template>
-    </q-table>
-    <q-table
-      class="m-[8px]"
-      bordered
-      title="Заметка(-и)"
-      :rows="rows"
-      @row-click="viewDetailedInfoAboutNote"
-      :columns="columns"
-      row-key="name"
-      hide-bottom
-    >
-      <template v-slot:body-cell-name="props">
-        <q-td :props="props">
-          <div
-            v-html="props.row.name"
-            class="max-w-[150px] whitespace-nowrap overflow-hidden text-ellipsis"
-          ></div>
-        </q-td>
-      </template>
-      <template v-slot:body-cell-description="props">
-        <q-td :props="props">
-          <div
-            v-html="props.row.description"
-            class="max-w-[150px] whitespace-nowrap overflow-hidden text-ellipsis"
-          ></div>
-        </q-td>
-      </template>
-      <template v-slot:body-cell-actions="props">
-        <q-td align="center">
-          <q-btn-dropdown @click.stop color="primary">
-            <q-list style="min-width: 100px">
-              <q-item clickable v-close-popup>
-                <q-item-section>
-                  <q-btn
-                    class="bg-amber-500 hover:bg-amber-600 text-white"
-                    icon="mdi-pin"
-                    size="sm"
-                    @click="pinNote(props.row)"
-                  />
-                </q-item-section>
-              </q-item>
-              <q-item clickable>
-                <q-item-section>
-                  <q-btn
-                    class="bg-blue-500 hover:bg-blue-600 text-white"
-                    icon="mdi-pencil"
-                    size="sm"
-                    @click="updateNote(props.row)"
-                  />
-                </q-item-section>
-              </q-item>
-              <q-item clickable v-close-popup>
-                <q-item-section>
-                  <q-btn
-                    class="bg-rose-500 hover:bg-rose-600 text-white"
-                    icon="mdi-delete"
-                    size="sm"
-                    @click="deleteNote(props.row)"
-                  />
-                </q-item-section>
-              </q-item>
-            </q-list>
-          </q-btn-dropdown>
-        </q-td>
-      </template>
-    </q-table>
+    />
+
     <DetailedInformationAboutNoteVue
       :isOpenDetailedInformation="isOpenDetailedInformation"
       @closeDetailedInformationSection="closeDetailedInformationSection"
@@ -148,6 +36,7 @@ import DetailedInformationAboutNoteVue from "../../components/organisims/Detaile
 import { useDateFormat } from "src/composables/javascript-function/formatDate";
 import { deleteMethod } from "src/composables/api-method/delete";
 import { putMethod } from "src/composables/api-method/put";
+import DocumentTable from "src/components/molecules/DocumentTable.vue";
 
 // global variables
 const { proxy } = getCurrentInstance();
@@ -178,44 +67,8 @@ const route = useRoute();
 const id = route.params.id;
 
 const rows = ref([]);
-const columns = computed(() => [
-  {
-    name: "title",
-    label: "Name",
-    align: "left",
-    field: (row) => row.title,
-    sortable: true,
-  },
-  {
-    name: "type",
-    label: "Type",
-    align: "left",
-    field: (row) => row.type,
-    sortable: true,
-  },
-  {
-    name: "created_at",
-    label: "Created At",
-    align: "left",
-    field: (row) => useDateFormat(row.createdAt),
-    sortable: true,
-  },
-  {
-    name: "updated_at",
-    label: "Updated At",
-    align: "left",
-    field: (row) => useDateFormat(row.updatedAt),
-    sortable: true,
-  },
-  {
-    name: "actions",
-    label: "Actions",
-    align: "center",
-    field: "id",
-  },
-]);
 
-const pinnedNote = ref([])
+const pinnedNote = ref([]);
 const getNotesById = async () => {
   try {
     const response = await getMethod(
@@ -232,7 +85,7 @@ const getNotesById = async () => {
       $q,
       "Заметки получены!"
     );
-    pinnedNote.value = pinned
+    pinnedNote.value = pinned;
   } catch (error) {
     console.error(error);
   }
@@ -242,10 +95,13 @@ onMounted(() => {
   getNotesById();
 });
 
+console.log(typeof pinnedNote.value);
+
 const isOpenDetailedInformation = ref(false);
 const detailedInformation = ref([]);
-const viewDetailedInfoAboutNote = (info, row) => {
+const viewDetailedInfoAboutNote = (evt, row, index) => {
   detailedInformation.value = row;
+
   isOpenDetailedInformation.value = true;
 };
 
