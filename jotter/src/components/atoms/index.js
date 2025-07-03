8adc152e79dd9ba8f48460bd24309864682e49e3
Weegoos <batyr.ashim05@mail.ru>
2025-07-03 11:48:25 +0500

    Jotter: The BaseInput.test.js has been created

--- jotter/src/components/atoms/index.js
+++ jotter/src/components/atoms/index.js
@@ -1,1 +1,2 @@
 export { default as Icon } from "./BaseIcon.vue";
+export { default as Input } from "./BaseInput.vue";
--- jotter/vitest.config.js
+++ jotter/vitest.config.js
@@ -1,22 +1,22 @@
-import { defineConfig } from 'vitest/config';
-import vue from '@vitejs/plugin-vue';
-import { quasar } from '@quasar/vite-plugin';
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from "vitest/config";
+import vue from "@vitejs/plugin-vue";
+import { quasar } from "@quasar/vite-plugin";
+import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
   plugins: [
     vue(),
     quasar({
-      sassVariables: 'src/css/quasar-variables.sass',
+      sassVariables: "src/css/quasar-variables.sass",
     }),
   ],
-   resolve: {
+  resolve: {
     alias: {
-      src: fileURLToPath(new URL('./src', import.meta.url)),
+      src: fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment: "jsdom",
   },
 });
