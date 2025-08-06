d3fc46833e68ea331c67ba68eb4087c616a61957
Weegoos <batyr.ashim05@mail.ru>
2025-08-06 21:53:34 +0500

    Jotter v3.0.3: The ViewDocument.vue has been updated

---
+++ jotter/src/components/atoms/BaseTooltip.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-tooltip anchor="bottom middle" self="top middle">
+  {{ props.label }}
+</q-tooltip>
+</template>
+
+<script setup>
+const props = defineProps({
+  label: [String, Number]
+})
+</script>
+
+<style>
+
+</style>
