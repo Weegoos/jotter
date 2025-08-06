82d3bf4ffed7f4cd07755a7fae22aa059dd222d2
Weegoos <batyr.ashim05@mail.ru>
2025-08-06 21:07:00 +0500

    Jotter v3.0.1: MoleculeCardToGetFile.vue has been created and integrated to the project

--- jotter/src/components/molecules/index.js
+++ jotter/src/components/molecules/index.js
@@ -3,3 +3,4 @@ export { default as Table } from './MoleculeTable.vue';
 export { default as Drawer } from './MoleculeDrawer.vue';
 export { default as Pagination } from './MoleculePagination.vue';
 export { default as Editor } from './MoleculeQEditor.vue';
+export { default as CardToGetFile } from './MoleculeCardToGetFile.vue';
