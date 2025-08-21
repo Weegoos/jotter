7b32bff0008c96963503bb0d1e6d158fa7434dd0
Weegoos <batyr.ashim05@mail.ru>
2025-08-21 15:13:05 +0500

    Jotter v3.27.0: The IGoalRepository.js has been updated
    Co-authored-by: Sanzhiiik <sanjar.edu2006@mail.ru>

--- server/src/domain/repositories/IGoalRepository.js
+++ server/src/domain/repositories/IGoalRepository.js
@@ -1,1 +1,41 @@
-export class IGoalRepository {}
+export class IGoalRepository {
+  /**
+   * Создать цель
+   * @param {Object} goalData
+   */
+  async create(goalData) {
+    console.log(goalData);
+    throw new Error('create function should be used');
+  }
+
+  /**
+   * Найти все цель по ID
+   * @param {Number} userId
+   */
+  async findAll(userId) {
+    console.log(userId);
+    throw new Error('findAll function should be used');
+  }
+
+  /**
+   * Частично обновить цель по ID
+   * @param {Object} goalData 
+   * @param {Number} goalId 
+   * @param {Number} userId 
+   */
+
+  async update(goalData, goalId, userId) {
+    console.log(goalData, goalId, userId);
+     throw new Error('update function should be used');
+  }
+  /**
+   * Удалить цель по ID
+   * @param {Number} goalId 
+   * @param {Number} userId 
+   */
+  async destroy(goalId, userId) {
+    console.log(goalId, userId);
+     throw new Error('destroy function should be used');
+    
+  }
+}
