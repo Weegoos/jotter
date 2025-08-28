4ee0eae79f22a7eeac3d67e1f0091fd97fc483c2
Weegoos <batyr.ashim05@mail.ru>
2025-08-28 11:32:45 +0500

    Jotter 3.31.13: GET /goals has been added to the frontend
    Co-authored-by: Sanzhiiik <sanjar.edu2006@mail.ru>

--- jotter/src/pages/Finance/TransactionComponent/ManageGoal.vue
+++ jotter/src/pages/Finance/TransactionComponent/ManageGoal.vue
@@ -2,22 +2,41 @@
   <div>
     <q-dialog v-model="isGoalDialogPage" persistent>
       <q-card>
-        <q-card-section class="grid grid-cols-3 gap-4">
-          <Input v-model="name" type="text" placeholder="Name"></Input>
-          <Input
-            v-model="target_amount"
-            placeholder="Target Amount"
-          ></Input>
-          <Input v-model="deadline" label="Deadline">
-            <template v-slot:append>
-              <q-icon name="event" class="cursor-pointer">
-                <q-popup-proxy transition-show="scale" transition-hide="scale">
-                  <q-date v-model="date" mask="YYYY-MM-DD" @update:model-value="onDateSelect" />
-                </q-popup-proxy>
-              </q-icon>
-            </template>
-          </Input>
+        <q-card-section>
+          <div class="grid grid-cols-3 gap-4">
+            <Input v-model="name" type="text" placeholder="Name"></Input>
+            <Input v-model="target_amount" placeholder="Target Amount"></Input>
+            <Input v-model="deadline" label="Deadline">
+              <template v-slot:append>
+                <q-icon name="event" class="cursor-pointer">
+                  <q-popup-proxy transition-show="scale" transition-hide="scale">
+                    <q-date v-model="date" mask="YYYY-MM-DD" @update:model-value="onDateSelect" />
+                  </q-popup-proxy>
+                </q-icon>
+              </template>
+            </Input>
+          </div>
+          <q-list bordered v-for="(goal, index) in goalsArray" :key="index" class="q-mt-sm">
+            <q-item clickable v-ripple>
+              <q-item-section>{{ goal.name }}</q-item-section>
+              <q-item-section avatar>
+                <Button class="text-black" flat icon="mdi-dots-vertical">
+                  <q-menu transition-show="scale" transition-hide="scale">
+                    <q-list >
+                      <q-item clickable @click="deleteCategory(category.id)">
+                        <q-item-section avatar>
+                          <q-icon color="red" name="mdi-delete" />
+                        </q-item-section>
+                        <q-item-section>Delete</q-item-section>
+                      </q-item>
+                    </q-list>
+                  </q-menu>
+                </Button>
+              </q-item-section>
+            </q-item>
+          </q-list>
         </q-card-section>
+
         <q-card-actions align="right">
           <q-btn flat label="Cancel" color="primary" @click="emit('closeDialog')" />
           <Button color="green" label="Create" @click="createGoal" />
@@ -30,8 +49,10 @@
 <script setup>
 import { useQuasar } from 'quasar';
 import { Button, Input } from 'src/components/atoms';
+import { getMethod } from 'src/composables/api-method/get';
 import { postMethod } from 'src/composables/api-method/post';
-import { getCurrentInstance, ref, watch } from 'vue';
+import { useWebSocket } from 'src/composables/javascript-function/websocket';
+import { getCurrentInstance, onMounted, ref, watch } from 'vue';
 
 // global variables
 const props = defineProps({
@@ -42,6 +63,19 @@ const props = defineProps({
 const { proxy } = getCurrentInstance();
 const serverURL = proxy.$serverURL;
 const $q = useQuasar();
+const webSocketURL = proxy.$webSocketURL;
+const socket = new WebSocket(webSocketURL);
+
+useWebSocket(webSocketURL);
+
+socket.onmessage = (event) => {
+  const data = JSON.parse(event.data);
+  const updateEvents = ['newGoal'];
+
+  if (updateEvents.includes(data.event)) {
+    getGoals();
+  }
+};
 
 const isGoalDialogPage = ref(props.isManageGoalOpen);
 
@@ -77,6 +111,20 @@ const createGoal = async () => {
     console.error(error);
   }
 };
+
+const goalsArray = ref([]);
+const getGoals = async () => {
+  try {
+    const response = await getMethod(serverURL, 'goals', $q);
+    goalsArray.value = response.goals;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+onMounted(() => {
+  getGoals();
+});
 </script>
 
 <style></style>
